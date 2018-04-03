59b3d14649bb7806cdabe28678aaa5ec431b2018
Anastassios Martakos <anastassios.martakos@gmail.com>
2018-04-03 22:29:12 +0200

    Fix logline highlighting not working for some input strings

--- src/app/search-results/search-result-details-modal/format-logline.pipe.ts
+++ src/app/search-results/search-result-details-modal/format-logline.pipe.ts
@@ -1,14 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { HighlightedWords } from './highlighted-words';
+import * as escapeStringRegexp from 'escape-string-regexp';
 
 @Pipe({
   name: 'formatLogline'
 })
 export class FormatLoglinePipe implements PipeTransform {
-  public transform (value: any, highlighted: HighlightedWords): any {
-    let formatted = value
-      .replace(/</g, '&lt;')
+  private escapeDiamonds (value: string): string {
+    return value.replace(/</g, '&lt;')
       .replace(/>/g, '&gt;');
+  }
+
+  public transform (value: any, highlighted: HighlightedWords): any {
+    let formatted = this.escapeDiamonds(value);
 
     const pieces = formatted.split(' ');
     const dateHostService = pieces.slice(0, 5).join(' ');
@@ -16,9 +20,11 @@ export class FormatLoglinePipe implements PipeTransform {
 
     formatted = `<span class="code-date-host-service">${dateHostService}</span> ${rest}`;
     highlighted.words.forEach((color, word) => {
+      const escapedWord = this.escapeDiamonds(word);
+
       formatted = formatted.replace(
-        new RegExp(word, 'g'),
-        `<span style=\"background-color: ${color.hex()};\">${word}</span>`);
+        new RegExp(escapeStringRegexp(escapedWord), 'gi'),
+        `<span style=\"background-color: ${color.hex()};\">${escapedWord}</span>`);
     });
 
     return formatted;
