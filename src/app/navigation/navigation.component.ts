d77526347a51be0e8e6e2947619c343651d68417
Anastassios Martakos <anastassios.martakos@gmail.com>
2018-05-26 23:36:20 +0200

    Add trend button to navigation if has trend data

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TrendStateService } from '../trend/trend-state-service/trend-state.service';
+import { isNullOrUndefined } from '../../utils/misc';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: [ './navigation.component.scss' ]
 })
-export class NavigationComponent implements OnInit {
-
-  constructor () {
+export class NavigationComponent {
+  public get hasTrendData (): boolean {
+    return !isNullOrUndefined(this._trendStateService.data);
   }
 
-  ngOnInit () {
+  public constructor (private _trendStateService: TrendStateService) {
   }
-
 }
